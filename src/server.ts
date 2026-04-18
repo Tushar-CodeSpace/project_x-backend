573d7e076f65490f9de0425a0cb52f4fc26c6605
Tushar-CodeSpace <tushar.codespace@gmail.com>
2026-04-18 14:28:50 +0530

    Update response message in root route to specify backend context

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World from the Backend!');
 });
 
 app.listen(port, () => {
